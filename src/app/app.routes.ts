8d7c25c1005609e66ba283d618af454c4941a83e
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 <muh123ybellah@gmail.com>
2026-02-18 10:09:26 +0200

    feat: add Home, Navbar, and NotFound components with basic structure and styles
    - Created Home component with its template and styles.
    - Created Navbar component with its template and styles.
    - Created NotFound component with its template and styles.
    - Added unit tests for Home, Navbar, and NotFound components.
    - Updated index.html to set language to Arabic and direction to RTL.
    - Minor formatting adjustments in main.ts.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home, title: 'home' },
+  {
+    path: 'blog',
+    // component: Blog,
+    loadComponent: () => import('./components/blog/blog').then((m) => m.Blog),
+    title: 'blog',
+  },
+  {
+    path: 'blog-details',
+    // component: BlogDetails,
+    loadComponent: () =>
+      import('./components/blog-details/blog-details').then((m) => m.BlogDetails),
+    title: 'blog-details',
+  },
+  {
+    path: 'about',
+    // component: About,
+    loadComponent: () => import('./components/about/about').then((m) => m.About),
+    title: 'about',
+  },
+  {
+    path: '**',
+    // component: NotFound,
+    loadComponent: () => import('./components/not-found/not-found').then((m) => m.NotFound),
+    title: 'not-found',
+  },
+];
